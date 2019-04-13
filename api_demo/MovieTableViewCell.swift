2797f375b8da4ca36eb0bf613f68507163f52d3b
John Morales <beljam@gmail.com>
2019-04-12 21:25:58 -0700

    Added custom cell and tableView data source functions. Successfully displays default data.

---
+++ api_demo/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  api_demo
+//
+//  Created by John on 4/12/19.
+//  Copyright © 2019 JAMorales. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dirNameLabel: UILabel!
+
+
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
--- api_demo/MovieTableViewController.swift
+++ api_demo/MovieTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MovieTableViewController: UITableViewController {
     let db = DataModel()
+    let cellId = "movieCell"
     var movieList: [Movie] = []
     
     @IBOutlet var movieTable: UITableView!
@@ -26,6 +27,13 @@ class MovieTableViewController: UITableViewController {
         return movieList.count
     }
 
-    // need cellForRowAt
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = movieTable.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! MovieTableViewCell
+        let movieData = movieList[indexPath.row]
+        cell.titleLabel.text = movieData.title
+        cell.dirNameLabel.text = movieData.director
+        return cell
+    }
+    
 }
 
